5d49ee13c27d7fc12344ae0f5f76b29f5f1de8cb
Mau reyes <reyes.marq@gmail.com>
2019-08-24 08:17:26 -0600

    redux dev tools, initial state

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,30 @@
 import { createStore } from "redux"
 
-const reducer = (state = {}, action) => {
+const reducer = (state, action) => {
+  if (action.type === 'GET_CHANNELS') {
+    return {
+      channels: [
+        { id: 1, name: 'Angular' },
+        { id: 2, name: 'Laravel' },
+        { id: 3, name: 'React' },
+      ]
+    }
+  }
   return state
 }
 
-const store = createStore(reducer)
+const initialState = {
+  channels: []
+}
+
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+store.dispatch({
+  type: 'GET_CHANNELS'
+})
 
 export default store
