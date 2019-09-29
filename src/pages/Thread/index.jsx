957d9a8a2c8f481db02f21a7c1cb0882ad660d76
Mau reyes <reyes.marq@gmail.com>
2019-09-29 11:58:17 -0600

    the thread replies

--- src/pages/Thread/index.jsx
+++ src/pages/Thread/index.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
 
 import { getThread } from '../../store/actions/threads'
+import { getReplies } from '../../store/actions/replies'
 
 import SingleThread from './SingleThread'
 import { strictEqual } from 'assert'
@@ -13,7 +14,9 @@ class ThreadContainer extends React.Component {
   }
 
   componentWillMount() {
-    this.props.getThread(this.props.match.params.id)
+    const { id } = this.props.match.params
+    this.props.getThread(id)
+    this.props.getReplies(id)
   }
 
   render() {
@@ -31,12 +34,17 @@ class ThreadContainer extends React.Component {
 
 const mapStateToProps = state => ({
   thread: state.thread.data,
-  loading: state.thread.loading
+  loading: state.thread.loading,
+  loadingReplies: state.thread.loading,
+  replies: state.thread.replies
 })
 
 const mapDispatchToProps = (dispatch) => ({
   getThread: (id) => {
     dispatch(getThread(id))
+  },
+  getReplies: (id) => {
+    dispatch(getReplies(id))
   }
 })
 
--- src/store/reducers/thread.js
+++ src/store/reducers/thread.js
@@ -1,21 +1,27 @@
 import { GET_THREAD, GET_THREAD_LOADED, GET_THREAD_LOADING } from '../actions/threads'
+import { GET_REPLIES, GET_REPLIES_LOADED } from '../actions/replies'
 
-const threadReducer = (state = { loading: true }, action) => {
+const threadReducer = (state = { loading: true, loadingReplies: true }, action) => {
   switch (action.type) {
     case GET_THREAD:
       return {
         ...state,
         data: action.payload
       }
-    case GET_THREAD_LOADING:
-      return {
-        ...state,
-        loading: true
-      }
     case GET_THREAD_LOADED:
       return {
         ...state,
         loading: false
+      }
+    case GET_REPLIES:
+      return {
+        ...state,
+        replies: action.payload
+      }
+    case GET_REPLIES_LOADED:
+      return {
+        ...state,
+        loadingReplies: false
       }
     default:
       return state
